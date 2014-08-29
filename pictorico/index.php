b2dce0ee97b02072ad4e4a85c73b3ae97ff9bfd3
Timo <timo@haragara.com>
2014-08-29 12:14:25 +0200

    test voor auto deplot

--- pictorico/index.php
+++ pictorico/index.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-	Test code voor upload versie 2
+	Test code voor upload versie 4 auto deploy test
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
