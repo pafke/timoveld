627bce2c4630ce332ccbdb7f4d0c22e636abca1c
Timo <timo@haragara.com>
2015-03-21 10:48:19 +0100

    Linkedin profiel toegevoegd

--- timo/index.php
+++ timo/index.php
@@ -39,6 +39,12 @@ get_header(); ?>
 				Maar voor een <em>interessant project</em>,<br>
 				Kun je altijd <a class="contact-scrollTo" href="#">contact</a> met mij opnemen.
 			</p>
+			<p>
+				<a href="">Download mijn CV</a>
+			</p>
+			<p>
+				<a href="nl.linkedin.com/in/timoveld" target="_blank" class="iconfont">&#xe801;</a>
+			</p>
 			<img src="<?php echo get_template_directory_uri(); ?>/img/self.svg" class="roundimg self">
 		</section>
 
