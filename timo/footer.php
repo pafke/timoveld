3989051af550fffdc9a8dc3d9f9ded77aa922769
Timo <timo@bigimpact.com>
2015-03-25 12:59:18 +0100

    Telefoon link disabled op desktop

--- timo/footer.php
+++ timo/footer.php
@@ -1,7 +1,17 @@
 		<section class="content contact">
 			<div class="contactform">
 				<h2>Contact</h2>
+				<?php
+				if ( wp_is_mobile() ) {
+				?>
 				<a href="tel:0643511371">T. 06 435 11 371</a>
+				<?php
+				} else {
+				?>
+				<a href="#">T. 06 435 11 371</a>
+				<?php
+				}
+				?>
 				<a href="mailto:mail@timoveld.nl">M. mail@timoveld.nl</a>
 				<p>
 					Voor vragen, eventuele samenwerking, of gewoon omdat het kan, mag je altijd contact met mij opnemen.
