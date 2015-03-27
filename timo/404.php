e926ecdca0cfc3c0659d4c75ddbb8bc1dbb0bdf8
Timo <timo@bigimpact.com>
2015-03-27 14:30:35 +0100

    404 update

--- timo/404.php
+++ timo/404.php
@@ -9,20 +9,20 @@
 				<span class="forcebreak">404</span>
 				Page not found!
 				<br>
-				<svg id="logo" version="1.1" viewBox="0 0 290 290"  >
-					<g>
-						<circle fill="none" class="outer" stroke="#FFF" stroke-width="2" stroke-miterlimit="10" cx="145" cy="145.001" r="141.501"/>
+				<a href="<?php echo home_url(); ?>">
+					<svg id="logo" version="1.1" viewBox="0 0 290 290"  >
 						<g>
-							<polygon fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10" points="145.041,205.997 45.23,106.188
-								244.852,106.188 		"/>
+							<circle fill="none" class="outer" stroke="#FFF" stroke-width="2" stroke-miterlimit="10" cx="145" cy="145.001" r="141.501"/>
+							<g>
+								<polygon fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10" points="145.041,205.997 45.23,106.188
+									244.852,106.188 		"/>
 
-								<line fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10" x1="145.041" y1="205.997" x2="145.041" y2="106.188"/>
+									<line fill="none" stroke="#FFFFFF" stroke-width="2" stroke-miterlimit="10" x1="145.041" y1="205.997" x2="145.041" y2="106.188"/>
+							</g>
 						</g>
-					</g>
-				</svg>
+					</svg>
+				</a>
 			</div>
 		</section>
-
-	
 	</body>
 </html>
