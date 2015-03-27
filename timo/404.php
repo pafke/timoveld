a173e1ab7c4363d495c904ee5aa3f05560c7165c
Timo <timo@bigimpact.com>
2015-03-27 14:40:00 +0100

    Font update 404 page

--- timo/404.php
+++ timo/404.php
@@ -2,6 +2,7 @@
 <html lang="nl">
 	<head>
 		<link rel='stylesheet' id='folder-style-css' href='<?php echo get_stylesheet_uri(); ?>' type='text/css' media='all' />
+		<link rel='stylesheet' id='fonts' href='http://fonts.googleapis.com/css?family=Raleway:700,800,100,400,200,300' type='text/css' media='all' />
 	</head>
 	<body>
 		<section class="fullscreen table maincolorBG">
