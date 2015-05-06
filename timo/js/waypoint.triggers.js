b43e391f403a20ed8bf9fae4cbfd3046c30320e1
Timo <timo@bigimpact.com>
2015-05-06 12:19:11 +0200

    Animatie timing aangepast en overbodige skill triggers verwijdert.

--- timo/js/waypoint.triggers.js
+++ timo/js/waypoint.triggers.js
@@ -3,30 +3,8 @@ var waypoint = new Waypoint({
 	handler: function(direction) {
 		if(direction == 'down'){
 			$('.logofixed').fadeIn(500);
-			$('.icon').addClass('black');
 		}else{
 			$('.logofixed').fadeOut(100);
-			$('.icon').removeClass('black');
-		}
-	}
-});
-var waypoint = new Waypoint({
-	element: document.getElementById('skillset-scrollTo'),
-	handler: function(direction) {
-		if(direction == 'down'){
-			$('.icon').removeClass('black');
-		}else{
-			$('.icon').addClass('black');
-		}
-	}
-});
-var waypoint = new Waypoint({
-	element: document.getElementById('werk-scrollTo'),
-	handler: function(direction) {
-		if(direction == 'down'){
-			$('.icon').addClass('black');
-		}else{
-			$('.icon').removeClass('black');
 		}
 	}
 });
